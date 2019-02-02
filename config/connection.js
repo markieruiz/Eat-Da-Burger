22e7687edda67843b1d26b6d4482d76bc51962f1
Markie Ruiz <markieruiz@Markies-MacBook-Pro.local>
2019-02-01 21:08:52 -0600

    testing

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,24 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'burgers_db'
+        host: 'd13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'd870tcsdwkr6alvf',
+        password: 'rdbjv63xrwxs6pfj',
+        database: 'bs2b6dpifximulvrd'
     });
 };
+
+
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'password',
+//         database: 'burgers_db'
+//     });
+// };
 
 connection.connect(function (err) {
     if (err) {
